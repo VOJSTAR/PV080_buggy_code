af4d91b6ccc032446c1fe8883c081a844b7806b7
unknown <xstarek2@ad.fi.muni.cz>
2025-04-30 14:52:34 +0200

    zkouska

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,7 +1,9 @@
 import sys 
-import os
 import yaml
 import flask
+
+
+import urllib
 
 app = flask.Flask(__name__)
 
@@ -44,6 +46,7 @@ def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
+
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
